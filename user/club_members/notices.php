c2a6be0bc85e1ac38c9128b10ac2fcb51fc6fcca
pradnyahaval <56785606+pradnyahaval@users.noreply.github.com>
2021-04-30 17:42:15 +0530

    Create notices.php

---
+++ user/club_members/notices.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/sidebar.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/task_notice_list.css">
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" data-auto-replace-svg="nest"></script>
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="header">
+        Club Management System
+        <div class="header-right">
+            <a class="#active" href="#contact"><img src="../images/message_icon.svg" alt="messages"></a>
+            <a class="#active" href="#about"><img src="../images/logout_icon.svg" alt="logout"></a>
+        </div>
+    </div>
+    
+    <div class="wrapper">
+        <div class="sidebar">
+            <h3>User Name</h3>
+            <a  href="userhome.php"><i class="fas fa-home"></i>Home</a>
+            <a  class="active" href="notices.php"><i class="far fa-flag"></i>Notices</a>
+            <a  href="task_list.php"><i class="fas fa-tasks"></i>Tasks</a>
+            <a  href="userprofile.php"><i class="fas fa-id-card"></i>Profile</a>     
+        </div>
+
+        <div class="main_containt">
+            <form class="notice_list" action="">
+                <h3>Notices</h3>
+                <table>
+                    <tr>
+                        <th>Notice Title</th>
+                        <th>Description</th>
+                        <th>Date</th>               
+                    </tr>
+                    <tr>
+                        <td>asd</td>
+                        <td>asd</td>
+                        <td>asd</td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+</body>
+</html>
